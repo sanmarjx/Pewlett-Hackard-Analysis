3f36f17f7ace5e8ecee98ea5c824651bba976ffd
Juan Sanmartin <sanmarjx21@gmail.com>
2022-12-17 13:24:01 -0500

    Uploaded Query_1 and retirement_info data

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -16,8 +16,9 @@ CREATE TABLE employees(
 	PRIMARY KEY (emp_no)
 );
 
+
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
+dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -35,23 +36,33 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_employees(
-	dept_no VARCHAR (4) NOT NULL,
+DROP TABLE dept_emp
+
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (dept_no, emp_no)
+  	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE titles(
+DROP TABLE titles
+
+CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
+	titles VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	PRIMARY KEY (emp_no, title)
+  	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, titles, from_date)
+	
 );
 
 SELECT * FROM departments
+SELECT * FROM dept_emp
+SELECT * FROM dept_manager
+SELECT * FROM employees
+SELECT * FROM salaries
+SELECT * FROM titles
